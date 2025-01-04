d330c75e568e2b250513432991c9b01efa113bd8
4n4k1n <anakin@pregitzer.de>
2025-01-05 00:18:32 +0100

    ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 11:50:58 by apregitz          #+#    #+#             */
+/*   Updated: 2025/01/05 00:00:15 by anakin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] && s2[i] && (s1[i] == s2[i]) && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char str1[5] = "tesT";
+// 	char str2[5] = "test";
+	
+// 	printf("%d", ft_strncmp(str1, str2, 4));
+// 	return (0);
+// }
