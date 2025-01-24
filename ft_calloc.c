06ddd63e376e8c65d6da831ee6d4f456367515b5
4n4k1n <anakin@pregitzer.de>
2025-01-24 14:42:28 +0100

    changed counter varible to 0 insteat of -1 (unsigned counter)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,26 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 13:51:49 by anakin            #+#    #+#             */
-/*   Updated: 2025/01/20 14:36:39 by anakin           ###   ########.fr       */
+/*   Updated: 2025/01/24 14:41:14 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    size_t i;
-    void *arr;
+	size_t i;
+	void *arr;
 
-    if (size == 0)
-        return (NULL);
-    arr = malloc(count * size);
-    i = -1;
-    while (++i < (size * count))
-        ((unsigned char *)arr)[i] = 0;
+	if (size == 0)
+		return (NULL);
+	arr = malloc(count * size);
+	i = 0;
+	while (i < (size * count))
+    {
+		((unsigned char *)arr)[i] = 0;
+        i++;
+    }
     return (arr);
 }
 
