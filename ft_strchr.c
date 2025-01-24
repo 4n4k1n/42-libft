092f2ed2092b127d713a578a8c60701c85ce85da
4n4k1n <anakin@pregitzer.de>
2025-01-24 14:55:49 +0100

    added the right function definition

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 21:27:47 by anakin            #+#    #+#             */
-/*   Updated: 2025/01/05 13:51:50 by anakin           ###   ########.fr       */
+/*   Updated: 2025/01/24 14:54:40 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strchr(char *str, int search_str)
+char	*ft_strchr(const char *s, int c)
 {
 	int		i;
 	int		save;
@@ -20,9 +20,9 @@ char	*ft_strchr(char *str, int search_str)
 
 	save = -1;
 	i = -1;
-	while (str[++i] != '\0')
+	while (s[++i] != '\0')
 	{
-		if (str[i] == search_str)
+		if (s[i] == c)
 			save = i;
 	}
 	if (save == -1)
@@ -31,12 +31,13 @@ char	*ft_strchr(char *str, int search_str)
 	if (!arr)
 		return (NULL);
 	i = -1;
-	while (str[(++i) + save])
-		arr[i] = str[i + save];
+	while (s[(++i) + save])
+		arr[i] = s[i + save];
 	arr[i] = '\0';
 	return (arr);
 }
 
+// #include <stdio.h>
 // int main (void)
 // {
 //     char *arr;
