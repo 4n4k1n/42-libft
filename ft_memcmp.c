653a9605fdb9da1410c9c1d140f3ecd0b2e1f3c7
4n4k1n <anakin@pregitzer.de>
2025-01-29 15:28:54 +0100

    ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 15:08:57 by anakin            #+#    #+#             */
+/*   Updated: 2025/01/29 15:24:38 by anakin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        if ((((unsigned char *)s1)[i]) != (((unsigned char *)s2)[i]))
+            return ((((unsigned char *)s1)[i]) - (((unsigned char *)s2)[i]));
+        i++;
+    }
+    return (0);
+}
+
+// int main(void)
+// {
+//     char *str1 = "test tesh!";
+//     char *str2 = "test teAt!";
+
+//     printf("%d\n", ft_memcmp(str1, str2, 0));
+//     return (0);
+// }
