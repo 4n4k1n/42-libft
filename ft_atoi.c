6b220e710b7d625f452df6a0006a364a617685d0
4n4k1n <anakin@pregitzer.de>
2025-02-03 15:57:56 +0100

    corrected the function

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,49 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:05:29 by apregitz          #+#    #+#             */
-/*   Updated: 2024/11/30 13:21:35 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/02/03 14:23:02 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	whitespaces(char *s, int num)
+int		ft_atoi(const char *nptr)
 {
-	if ((*s == ' ' || *s == '\n' || *s == '\t' || *s == '\r' \
-	|| *s == '\v' || *s == '\f' || *s == '+') && num == 0)
-	{
-		return (1);
-	}
-	return (0);
+    long int    number;
+    int         negativ;
+
+    while ((*nptr >= 9 && *nptr <= 13) || *nptr == ' ')
+        nptr++;
+    negativ = 1;
+    if (*nptr == '-' || *nptr == '+')
+    {
+        if (*nptr == '-')
+            negativ = -1;
+        nptr++;
+    }
+    number = 0;
+    while (*nptr >= '0' && *nptr <= '9')
+    {
+        number = (number * 10) + ((*nptr - '0') * negativ);
+        if (number > 2147483647)
+            return (2147483647);
+        else if (number < -2147483648)
+            return (-2147483648);
+        nptr++;
+    }
+    return ((int )number);
 }
 
-int	ft_atoi(char *str)
-{
-	int	negativ;
-	int	num;
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void)
+// {
+//     char *str = "   - 214ghasd7483647";
 
-	negativ = 1;
-	num = 0;
-	while (whitespaces(str, num))
-		str++;
-	while (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			negativ *= -1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		num = (num * 10) + ((*str - '0') * negativ);
-		str++;
-	}
-	return (num);
-}
-
-/* #include <stdio.h>
-int main()
-{
-    printf("%d\n", ft_atoi(" -2147483648"));
-    return 0;
-} */
+//     printf("atoi    = %d\n", atoi(str));
+//     printf("ft_atoi = %d\n", ft_atoi(str));
+//     return (0);
+// }
