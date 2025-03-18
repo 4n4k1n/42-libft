4d8584344fd086ad974ad00fe097e5e5c941691a
4n4k1n <anakin@pregitzer.de>
2025-03-18 21:08:33 +0100

    removed the NULL check

--- src/extra_src/ft_swp.c
+++ src/extra_src/ft_swp.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_swp.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:46:48 by apregitz          #+#    #+#             */
-/*   Updated: 2025/03/12 13:51:02 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/03/18 20:31:54 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "libft.h"
 #include <stdio.h>
 
-int	ft_swap(void *arr1, void *arr2, size_t size)
+void	ft_swap(void *arr1, void *arr2, size_t size)
 {
 	size_t			i;
 	unsigned char	*cpy1;
 	unsigned char	*cpy2;
 
-	if (!arr1 || !arr2 || !size)
-		return (0);
 	cpy1 = (unsigned char *)arr1;
 	cpy2 = (unsigned char *)arr2;
 	i = 0;
@@ -31,17 +29,15 @@ int	ft_swap(void *arr1, void *arr2, size_t size)
 		cpy1[i] ^= cpy2[i];
 		i++;
 	}
-	return (1);
 }
 
-// int main(void)
-// {
-// 	char arr1[] = "testhallo";
-// 	char arr2[] = "1234";
-// 	printf("%s\n", arr1);
-// 	printf("%s\n", arr2);
-// 	ft_swap(arr1, arr1+4, 7);
-// 	printf("%s\n", arr1);
-// 	printf("%s\n", arr2);
-// 	return (0);
-// }
+int main(void)
+{
+	int arr1 = 1;
+	printf("%d\n", arr1);
+	printf("%d\n", arr1);
+	ft_swap(&arr1, &arr1, 4);
+	printf("%d\n", arr1);
+	printf("%d\n", arr1);
+	return (0);
+}
